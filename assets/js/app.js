65d674d32ad40024ce81dc50cfcef2685ce2d156
Abdul Rafi <arafi6624@bths.edu>
2022-08-27 20:36:06 -0400

    Website complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,3 +40,19 @@ async function handleSubmit(event) {
 }
 form.addEventListener("submit", handleSubmit)
 
+const video = document.querySelector('video');
+let isPaused = false;
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio !== 1 && !video.paused) {
+      video.pause();
+      isPaused = true;
+    } else if (isPaused) {
+      video.play();
+      isPaused = false
+    }
+  });
+}, {
+  threshold: 1
+});
+observer.observe(video);
